4039455a6b38ab04d01d5a80caebc060b3cbc4dd
Alex <alexstocki22@gmail.com>
2021-08-27 19:34:03 -0300

    reusable hook

--- practice-react/examples-react/src/App.js
+++ practice-react/examples-react/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
-import EventHandler from './components/EventHandler';
+// import EventHandler from './components/EventHandler';
 import InputLocalStorage from './components/InputLocalStorage';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    // <EventHandler />
-    <InputLocalStorage />
+      // <EventHandler />
+    <div id='container'>
+      <button onClick={() => setCount(c => c + 1)}>{count}</button>
+      <InputLocalStorage />
+    </div>
   );
 }
 
--- practice-react/examples-react/src/components/InputLocalStorage.js
+++ practice-react/examples-react/src/components/InputLocalStorage.js
@@ -1,12 +1,22 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReusableHook from "./ReusableHook";
 
 const InputLocalStorage = () => {
     // passing an arrow function to localStorage makes that the function is only
     // called at the first render
-    const [name, setName] = useState(() => window.localStorage.getItem('name') || '')
+    // ---------- start of code reused in ReusableHook.js ----------
+    // // const [name, setName] = useState(() => window.localStorage.getItem('name') || '')
 
-    useEffect(() => window.localStorage.setItem('name', name))
+    // the second parameter makes the sync of the element
+    // without the 'name' on the array the changes would be lost
+    // // useEffect(() => {
+    // //     console.log('Entrando en el useEffect')
+    // //     window.localStorage.setItem('name', name)
+    // // }, [name])
+    // ---------- end of code reused in ReusableHook.js ----------
+
+    const [name, setName] = ReusableHook('name', '')
 
     const handleChange = (event) => setName(event.target.value)
 
